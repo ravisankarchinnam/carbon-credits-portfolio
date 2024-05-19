55a65b46aac29b794ef41c559482f0d0bad68a23
ravisankarchinnam <22351907+ravisankarchinnam@users.noreply.github.com>
2024-05-19 16:55:30 +0200

    [UI] cleanup

--- apps/ui/src/app/page.tsx
+++ apps/ui/src/app/page.tsx
@@ -1,10 +1,9 @@
-import styles from "./page.module.css";
 import { AppContent } from "@/components/AppContent";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <AppContent />  
+    <main>
+      <AppContent />
     </main>
   );
 }
--- apps/ui/src/components/AppContent.tsx
+++ apps/ui/src/components/AppContent.tsx
@@ -1,16 +1,14 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const AppContent = () => {
-    const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch('/api')
-        .then(response => response.text())
-        .then(setMessage)
-    }, []);
-  return (
-    <div>{message}</div>
-  )
-}
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.text())
+      .then(setMessage);
+  }, []);
+  return <div>{message}</div>;
+};
