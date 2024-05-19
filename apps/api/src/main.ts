ddcac0a736b2f8d340d7d6a71f7ee2b829f0e1a0
ravisankarchinnam <22351907+ravisankarchinnam@users.noreply.github.com>
2024-05-19 17:45:51 +0200

    [API] configure swagger for documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as pkg from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(Number(process.env.PORT) || 4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
